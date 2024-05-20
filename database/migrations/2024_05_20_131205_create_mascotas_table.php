f5ebb4ad20a71318554da8cac23c2af57877fc8f
Jose  Quispe <jkqr2016@gmail.com>
2024-05-20 08:24:19 -0500

    Agregando migracion mascotas

---
+++ database/migrations/2024_05_20_131205_create_mascotas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->char('sexo', 6);
+            $table->string('color');
+            $table->tinyInteger('pedigree', 1);
+            $table->date('fecha_nacimiento')->nullable();
+            $table->string('raza')->nullable();
+            $table->string('imagen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mascotas');
+    }
+};
