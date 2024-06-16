3d9058b03a07f9c003d8e1f841d6175c635821f8
Jose  Quispe <jkqr2016@gmail.com>
2024-06-16 02:27:40 -0500

    AVANCE

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
+    Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
+    Route::post('/clientes/store', [ClienteController::class, 'store'])->name('clientes.store');
 
-Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index');
-Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
-Route::post('/clientes/store', [ClienteController::class, 'store'])->name('clientes.store');
+    //Medicamentos
+    Route::get('/medicamentos', [MedicamentoController::class, 'index'])->name('medicamentos.index');
+    Route::get('/medicamentos/create', [MedicamentoController::class, 'create'])->name('medicamentos.create');
 
-//Medicamentos
-Route::get('/medicamentos', [MedicamentoController::class, 'index'])->name('medicamentos.index');
-Route::get('/medicamentos/create', [MedicamentoController::class, 'create'])->name('medicamentos.create');
-
-// Mascotas
-Route::get('/mascotas', [MascotaController::class, 'index'])->name('mascotas.index');
+    // Mascotas
+    Route::get('/mascotas', [MascotaController::class, 'index'])->name('mascotas.index');
+});
