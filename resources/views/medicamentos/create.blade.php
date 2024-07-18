4def7f4eb374e7f45696d6f5ae1401b44554e48d
Jose  Quispe <jkqr2016@gmail.com>
2024-07-18 09:42:09 -0500

    Agregando vista para crear registro de medicamentos

--- resources/views/medicamentos/create.blade.php
+++ resources/views/medicamentos/create.blade.php
@@ -2,23 +2,38 @@
 
 
 @section('contenido')
-<div class="card">
+<div class="card mt-8 mb-8 max-w-screen-md m-auto">
     <h2>Registro de Medicamentos:</h2>
-    <form action="/registrar_med" method="POST">
+    <form class="form" action="/registrar_med" method="POST">
         @csrf
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required>
+        <div class="form__group">
+            <div class="form__input-group">
+                <label class="form__label" for="nombre">Nombre:</label>
+                <input class="form__input" type="text" id="nombre" name="nombre" required>
+            </div>
+            <div class="form__input-group">
+                <label class="form__label" for="apellido">Marca:</label>
+                <input class="form__input" type="text" id="apellido" name="apellido" required>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__input-group">
+                <label class="form__label" for="n_documento">Stock:</label>
+                <input class="form__input" type="text" id="n_documento" name="n_documento" required>
+            </div>
+            <div class="form__input-group">
+                <label class="form__label" for="sexo">Precio:</label>
+                <input class="form__input" type="text" id="n_documento" name="n_documento" required>
+            </div>
+        </div>
+        <div>
+            <div class="form__input-group">
+                <label class="form__label" for="descripcion">Descripcion:</label>
+                <textarea class="form__input" id="descripcion" name="descripcion"></textarea>
+            </div>
+        </div>
 
-        <label for="apellido">Marca:</label>
-            <input type="text" id="apellido" name="apellido" required>
-
-            <label for="n_documento">Stock:</label>
-            <input type="text" id="n_documento" name="n_documento" required>
-
-            <label for="sexo">Precio:</label>
-            <input type="text" id="n_documento" name="n_documento" required>
-
-        <button type="submit">Registrar Medicamento</button>
+        <button class="form__button-submit" type="submit">Registrar Medicamento</button>
     </form>
 </div>
 @endsection
