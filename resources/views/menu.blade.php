fa32d2a7c36553abd9944cca8380933660c27f24
Jose Quispe <jkqr2016@gmail.com>
2024-07-24 17:59:24 -0500

    Adaptando enlaces de sidebars

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 <div class="app">
     <div class="sidebar-container" id="sidebar">
-        <div class="sidebar-container__header">
+        <header class="sidebar-container__header">
             <div class="sidebar-container__title">
                 <div class="sidebar-container__header__logo">
                     <i class="ri-medicine-bottle-fill"></i>
@@ -13,22 +13,22 @@
             <div class="sidebar-container__hamburguer" id="sidebar-close">
                 <i class="ri-menu-line"></i>
             </div>
-        </div>
-        <div class="sidebar-container__list">
-            <div class="sidebar-container__list__item"><a href=""><i class="ri-layout-5-fill"></i> Dashboard</a></div>
-            <div class="sidebar-container__list__item"><a href="{{ route('clientes.index') }}"><i class="ri-user-fill"></i> Clientes</a></div>
-            <div class="sidebar-container__list__item"><a href="{{ route('mascotas.index') }}"><i class="ri-stethoscope-fill"></i> Mascotas</a></div>
-            <div class="sidebar-container__list__item"><a href=""><i class="ri-clipboard-fill"></i> H. Clinico</a></div>
-            <div class="sidebar-container__list__item"><a href="{{ route('medicamentos.index') }}"><i class="ri-medicine-bottle-fill"></i> Medicamentos</a></div>
-            <div class="sidebar-container__list__item"><a href=""><i class="ri-scissors-2-fill"></i> Estetica</a></div>
-            <div class="sidebar-container__list__item"><a href=""><i class="ri-money-dollar-circle-fill"></i> Ventas</a></div>
-            <div class="sidebar-container__list__item">
+        </header>
+        <ul class="sidebar-container__list">
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href=""><i class="ri-layout-5-fill"></i> Dashboard</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href="{{ route('clientes.index') }}"><i class="ri-user-fill"></i> Clientes</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href="{{ route('mascotas.index') }}"><i class="ri-stethoscope-fill"></i> Mascotas</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href=""><i class="ri-clipboard-fill"></i> H. Clinico</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href="{{ route('medicamentos.index') }}"><i class="ri-medicine-bottle-fill"></i> Medicamentos</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href=""><i class="ri-scissors-2-fill"></i> Estetica</a></li>
+            <li class="sidebar-container__list__item"><a class="sidebar-container__enlace" href=""><i class="ri-money-dollar-circle-fill"></i> Ventas</a></li>
+            <li class="sidebar-container__list__item">
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
-                    <button type="submit" href=""><i class="ri-door-open-fill"></i> Logout</button>
+                    <button class="sidebar-container__enlace"  type="submit" href=""><i class="ri-door-open-fill"></i> Logout</button>
                 </form>
-            </div>
-        </div>
+            </li>
+        </ul>
     </div>
     <div class="cotenido-principal">
         <nav class="nav">
@@ -37,9 +37,9 @@
                 <i class="ri-menu-line"></i>
             </div>
         </nav>
-        <div class="mx-auto" style="width: 95%">
+        <main class="mx-auto" style="width: 95%">
             @yield('contenido')
-        </div>
+        </main>
     </div>
 </div>
 
