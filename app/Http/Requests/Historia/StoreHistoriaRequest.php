55e6f4e20a41d9cf72a43a5f6deb4ca715efc5b6
Jose  Quispe <jkqr2016@gmail.com>
2024-08-29 12:29:00 -0500

    Agregando validacion alfanumerica en registro de historias clinicas

--- app/Http/Requests/Historia/StoreHistoriaRequest.php
+++ app/Http/Requests/Historia/StoreHistoriaRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Historia;
 
+use App\Rules\AlphaNumericUnicode;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreHistoriaRequest extends FormRequest
@@ -23,13 +24,13 @@ class StoreHistoriaRequest extends FormRequest
     {
         return [
             'numero' => ['required','integer','unique:historias_clinicas,numero'],
-            'motivo' => ['nullable','string'],
-            'mucosas' => ['nullable', 'string'],
-            'amnanesis' => ['nullable', 'string'],
-            'diagnostico' => ['nullable', 'string'],
-            'tratamiento' => ['nullable', 'string'],
-            'precio' => ['nullable', 'decimal:2,5'],
-            'peso' => ['nullable', 'decimal:2,4'],
+            'motivo' => ['nullable','string', new AlphaNumericUnicode],
+            'mucosas' => ['nullable', 'string', new AlphaNumericUnicode],
+            'amnanesis' => ['nullable', 'string', new AlphaNumericUnicode],
+            'diagnostico' => ['nullable', 'string', new AlphaNumericUnicode],
+            'tratamiento' => ['nullable', 'string', new AlphaNumericUnicode],
+            'precio' => ['nullable', 'numeric', 'min:0.01', 'max:1000', 'regex:/^\d+(\.\d{1,2})?$/'],
+            'peso' => ['nullable', 'numeric', 'min:0.01', 'max:120', 'regex:/^\d+(\.\d{1,2})?$/'],
             'fecha' => ['required', 'date'],
             'mascota_id' => ['required', 'integer']
         ];
