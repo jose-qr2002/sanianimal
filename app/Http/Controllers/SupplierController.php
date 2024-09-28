d5d7af151cc8c27e0c78bbdc51121a788e4c74de
Belinda Arocutipa <1478562@senati.pe>
2024-09-28 12:38:52 -0500

    SAN-14 ADD: Controllador con index proveedores

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Supplier;
+
+class SupplierController extends Controller
+{
+    public function index()
+    {
+        $suppliers = Supplier::paginate(10);
+        return view('suppliers.index', compact('suppliers'));
+    }
+}
