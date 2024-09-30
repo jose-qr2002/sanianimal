5631e0e0c1e2ca13d94c32cba9620739eb909c33
AlexanderSenati <1443935@senati.pe>
2024-09-30 03:13:10 -0500

    SAN-28-Metodo-store-proveedores

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -17,4 +17,18 @@ class SupplierController extends Controller
     {
         return view('suppliers.create');
     }
+
+    public function store(Request $request)
+{
+    $request->validate([
+        'ruc' => 'required|digits:11|unique:suppliers,ruc',
+        'name' => 'required|string|max:255',
+        'phone' => 'required|digits:9',
+        'address' => 'required|string|max:255',
+    ]);
+
+    Supplier::create($request->all());
+
+    return redirect()->route('suppliers.index')->with('success', 'Proveedor registrado correctamente.');
+}
 }
