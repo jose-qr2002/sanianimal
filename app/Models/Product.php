a3576be3249b6db747c8966883c409caa9924dc7
Jose  Quispe <jkqr2016@gmail.com>
2024-11-09 10:21:51 -0500

    SAN-51 Agregando modelos y controladores de categorias y productos

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'brand',
+        'measurement',
+        'image',
+        'supplier_id',
+        'category_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+}
